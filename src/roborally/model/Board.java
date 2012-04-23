ce7d84d7d3d1b554abeaa0eafa0a84d4bd2d202f
Bavo Goosens <bavo.goosens@gmail.be>
2012-04-23 17:44:26 +0200

    board gewijzigd

--- src/roborally/model/Board.java
+++ src/roborally/model/Board.java
@@ -1,5 +1,26 @@
 package roborally.model;
 
 public class Board {
+	
+	private final long width;
+	private final long height;
+	
+	private final long UPPER_BOUND_WIDTH = Long.MAX_VALUE;
+	private final long LOWER_BOUND_WIDTH = 0;
+	private final long UPPER_BOUND_HEIGTH = Long.MAX_VALUE;
+	private final long LOWER_BOUND_HEIGTH = 0;
+	
+	private Map <Position, > contains;
+	
+	
+	
+	
+	public boolean isValidHeight(long height){
+		return height > LOWER_BOUND_HEIGHT && height <= UPPER_BOUND_HEIGHT;
+	}
+	
+	public boolean isValidWidth(long width){
+		return width > LOWER_BOUND_WIDTH && width <= UPPER_BOUND_WIDTH
+	}
 
 }
