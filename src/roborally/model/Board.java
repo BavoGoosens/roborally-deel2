d1a7a38f1823d50476dcff09bbc070748f3b802e
Bavo Goosens <bavo.goosens@gmail.be>
2012-04-25 15:31:18 +0200

    random facade

--- src/roborally/model/Board.java
+++ src/roborally/model/Board.java
@@ -1,26 +1,26 @@
 package roborally.model;
 
 public class Board{
-	/*
+	
 	private final long width;
 	private final long height;
 	
-	private final long UPPER_BOUND_WIDTH = Long.MAX_VALUE;
-	private final long LOWER_BOUND_WIDTH = 0;
-	private final long UPPER_BOUND_HEIGTH = Long.MAX_VALUE;
-	private final long LOWER_BOUND_HEIGTH = 0;
+	private static final long UPPER_BOUND_WIDTH = Long.MAX_VALUE;
+	private static final long LOWER_BOUND_WIDTH = 0;
+	private static final long UPPER_BOUND_HEIGTH = Long.MAX_VALUE;
+	private static final long LOWER_BOUND_HEIGTH = 0;
 	
-	
-	
-	
-	
+	public Board (long height, long width){
+		this.height = height;
+		this.width = width;
+	}
 	
 	public boolean isValidHeight(long height){
-		return height > LOWER_BOUND_HEIGHT && height <= UPPER_BOUND_HEIGHT;
+		return (height > Board.LOWER_BOUND_HEIGTH) && (height <= Board.UPPER_BOUND_HEIGTH);
 	}
 	
 	public boolean isValidWidth(long width){
-		return width > LOWER_BOUND_WIDTH && width <= UPPER_BOUND_WIDTH
+		return (width > Board.LOWER_BOUND_WIDTH) && (width <= Board.UPPER_BOUND_WIDTH);
 	}
-	*/
+	
 }
--- src/roborally/model/Facade.java
+++ src/roborally/model/Facade.java
@@ -1,119 +1,120 @@
 package roborally.model;
 
+import roborally.basics.Energy;
+import roborally.basics.Weight;
 import roborally.interfaces.IFacade;
 
 import java.util.Set;
 
 
-public class Facade implements IFacade{
+public class Facade implements IFacade<Board, Robot, Wall, Battery>{
 	
 		
 	public Facade(){}
 
 	@Override
-	public Object createBoard(long width, long height) {
+	public Board createBoard(long width, long height) {
+		Board board = new Board(width,height);
+		return board;
+	}
+
+	@Override
+	public void merge(Board board1, Board board2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Battery createBattery(double initialEnergy, int weight) {
+		Battery battery = new Battery(new Energy((int)initialEnergy), new Weight(weight));
+		return battery;
+	}
+
+	@Override
+	public void putBattery(Board board, long x, long y, Battery battery) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public long getBatteryX(Battery battery) throws IllegalStateException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public long getBatteryY(Battery battery) throws IllegalStateException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Robot createRobot(int orientation, double initialEnergy) {
+		Robot robot = new Robot(new Orientation())
+	}
+
+	@Override
+	public void putRobot(Board board, long x, long y, Robot robot) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public long getRobotX(Robot robot) throws IllegalStateException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public long getRobotY(Robot robot) throws IllegalStateException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getOrientation(Robot robot) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getEnergy(Robot robot) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void move(Robot robot) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turn(Robot robot) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Set<Battery> getPossessions(Robot robot) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public void merge(Object board1, Object board2) {
+	public void pickUp(Robot robot, Battery battery) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public Object createBattery(double initialEnergy, int weight) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void putBattery(Object board, long x, long y, Object battery) {
+	public void use(Robot robot, Battery battery) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public long getBatteryX(Object battery) throws IllegalStateException {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public long getBatteryY(Object battery) throws IllegalStateException {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public Object createRobot(int orientation, double initialEnergy) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void putRobot(Object board, long x, long y, Object robot) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public long getRobotX(Object robot) throws IllegalStateException {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public long getRobotY(Object robot) throws IllegalStateException {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int getOrientation(Object robot) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public double getEnergy(Object robot) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void move(Object robot) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void turn(Object robot) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Set getPossessions(Object robot) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void pickUp(Object robot, Object battery) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void use(Object robot, Object battery) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void drop(Object robot, Object battery) {
+	public void drop(Robot robot, Battery battery) {
 		// TODO Auto-generated method stub
 		
 	}
@@ -125,7 +126,7 @@ public class Facade implements IFacade{
 	}
 
 	@Override
-	public double getMinimalCostToReach(Object robot, long x, long y) {
+	public double getMinimalCostToReach(Robot robot, long x, long y) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
@@ -137,60 +138,61 @@ public class Facade implements IFacade{
 	}
 
 	@Override
-	public void moveNextTo(Object robot, Object other) {
+	public void moveNextTo(Robot robot, Robot other) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void shoot(Object robot) throws UnsupportedOperationException {
+	public void shoot(Robot robot) throws UnsupportedOperationException {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public Object createWall() throws UnsupportedOperationException {
+	public Wall createWall() throws UnsupportedOperationException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public void putWall(Object board, long x, long y, Object wall)
+	public void putWall(Board board, long x, long y, Wall wall)
 			throws UnsupportedOperationException {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public long getWallX(Object wall) throws IllegalStateException,
+	public long getWallX(Wall wall) throws IllegalStateException,
 			UnsupportedOperationException {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
-	public long getWallY(Object wall) throws IllegalStateException,
+	public long getWallY(Wall wall) throws IllegalStateException,
 			UnsupportedOperationException {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
-	public Set getRobots(Object board) {
+	public Set<Robot> getRobots(Board board) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Set getBatteries(Object board) {
+	public Set<Battery> getBatteries(Board board) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Set getWalls(Object board) throws UnsupportedOperationException {
+	public Set<Wall> getWalls(Board board) throws UnsupportedOperationException {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 }
+
+	
