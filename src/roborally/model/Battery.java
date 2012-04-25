8192c54ee1ead90a72721d30e7d39c0e3b3e167d
Samuel Debruyn <samuel.debruyn@gmail.com>
2012-04-25 13:33:31 +0200

    Muren toegevoegd

--- src/roborally/model/Battery.java
+++ src/roborally/model/Battery.java
@@ -1,5 +1,5 @@
 package roborally.model;
 
 public class Battery {
-
+	
 }
--- src/roborally/model/Wall.java
+++ src/roborally/model/Wall.java
@@ -1,5 +1,40 @@
 package roborally.model;
 
-public class Wall {
+import be.kuleuven.cs.som.annotate.Basic;
+import roborally.basics.Position;
 
+/**
+ * Deze klasse houdt een muur bij. Deze heeft een positie.
+ * 
+ * @author 	Bavo Goosens (1e bachelor informatica, r0297884), Samuel Debruyn (1e bachelor informatica, r0305472)
+ * 
+ * @version 1.0
+ */
+public class Wall {
+	
+	private final Position position;
+	
+	/**
+	 * Deze methode maakt een nieuwe muur aan op een gegeven positie.
+	 * 
+	 * @param	position
+	 * 			De positie van de nieuwe muur.
+	 * 
+	 * @post	new.position == position
+	 */
+	public Wall(Position position){
+		this.position = position;
+	}
+	
+	/**
+	 * Geeft de positie van de muur terug.
+	 * 
+	 * @return 	Position position
+	 * 			|new.position
+	 */
+	@Basic
+	public Position getPosition() {
+		return position;
+	}
+	
 }
