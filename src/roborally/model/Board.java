3d8efb0097e6f3c0e5a89498bf584797dd660cb4
Bavo Goosens <bavo.goosens@gmail.be>
2012-04-26 18:11:02 +0200

    strange

--- src/roborally/model/Board.java
+++ src/roborally/model/Board.java
@@ -10,7 +10,7 @@ public class Board{
 	
 	private final long width;
 	private final long height;
-	private HashMap <Position,HashSet> map;
+	private HashMap <Position,HashSet> map = new HashMap<Position,HashSet>();
 	
 	private static final long UPPER_BOUND_WIDTH = Long.MAX_VALUE;
 	private static final long LOWER_BOUND_WIDTH = 0;
@@ -21,20 +21,17 @@ public class Board{
 		this.height = height;
 		this.width = width;
 	}
-	
-	public boolean isValidHeight(long height){
-		return (height > Board.LOWER_BOUND_HEIGTH) && (height <= Board.UPPER_BOUND_HEIGTH);
-	}
-	
-	public boolean isValidWidth(long width){
-		return (width > Board.LOWER_BOUND_WIDTH) && (width <= Board.UPPER_BOUND_WIDTH);
-	}
 
 	public void putBattery(long x, long y, Battery battery) {
 		Position key = new Position(x,y);
 		if (!map.containsValue(battery)){
 			if (isPlacableOnPosition(key,battery)){
-				
+				if (map.get(key) == null){
+					HashSet<Object> input = new HashSet<Object>();
+					input.add(battery);
+					map.put(key,input);
+				}
+				map.get(key).add(battery);
 			}
 		}
 	}
@@ -42,21 +39,43 @@ public class Board{
 	public HashSet<Object> getObjectsOnPosition(Position pos){
 		return this.map.get(pos);
 	}
+	
 	public boolean isPlacableOnPosition(Position pos , Object obj){
+		if (this.map.get(pos) == null)
+			return true;
 		if (this.map.get(pos).isEmpty())
 			return true;
-		//if(this.map.get(pos))
+		if (this.map.get(pos).size() == 1){
+			Object derp = this.map.get(pos).iterator().next();
+			if (derp instanceof Wall)
+				return false;
+		}
 		return false;
 	}
 
 	public void putRobot(long x, long y, Robot robot) {
-		// TODO Auto-generated method stub
-		
+		Position key = new Position(x,y);
+		if (!map.containsValue(robot)){
+			if (isPlacableOnPosition(key,robot)){
+				if (map.get(key) == null){
+					HashSet<Object> input = new HashSet<Object>();
+					input.add(robot);
+					map.put(key,input);
+				}
+				map.get(key).add(robot);
+			}
+		}
 	}
 
 	public void putWall(long x, long y, Wall wall) {
-		// TODO Auto-generated method stub
-		
+		Position key = new Position(x,y);
+		if (!map.containsValue(wall)){
+			if (isPlacableOnPosition(key,wall)){
+				HashSet<Object> input = new HashSet<Object>();
+				input.add(wall);
+				map.put(key,input);
+			}
+		}
 	}
 
 	public HashSet<Robot> getRobots() {
@@ -101,4 +120,20 @@ public class Board{
 		return result;
 	}
 	
+
+	public boolean isValidHeight(long height){
+		return (height > Board.LOWER_BOUND_HEIGTH) && (height <= Board.UPPER_BOUND_HEIGTH);
+	}
+	
+	public boolean isValidWidth(long width){
+		return (width > Board.LOWER_BOUND_WIDTH) && (width <= Board.UPPER_BOUND_WIDTH);
+	}
+
+	public long getWidth() {
+		return width;
+	}
+
+	public long getHeight() {
+		return height;
+	}
 }
