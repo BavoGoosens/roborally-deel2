ef31a17a30f11309b421ffbaf7d90eb91946b03d
Bavo Goosens <bavo.goosens@gmail.be>
2012-05-03 02:17:35 +0200

    doc comperators

--- src/roborally/utils/PositionPairComparatorDistance.java
+++ src/roborally/utils/PositionPairComparatorDistance.java
@@ -2,8 +2,28 @@ package roborally.utils;
 
 import java.util.Comparator;
 
+/**
+ * Klasse met een speciale comperator in om te kunnen sorteren op de manhattanDistance van een positiepaar.
+ * 
+ * @author 	Bavo Goosens (1e bachelor informatica, r0297884), Samuel Debruyn (1e bachelor informatica, r0305472)
+ *
+ * @version 1.0
+ *  
+ */
 public class PositionPairComparatorDistance implements Comparator<PositionPair>{
 
+	/**
+	 * Methode die een integer teruggeeft die aangeeft of positionpair1 zijn manahattandistance groter(1), kleiner(-1) of gelijk(0) is dan een tweed positiepaar.
+	 * 
+	 * @return 	int result 
+	 * 			|if(pp1.getManhattanDistance() > pp2.getManhattanDistance()){
+	 *				result ==  1;
+	 *			}else if(pp1.getManhattanDistance() < pp2.getManhattanDistance()){
+	 *				result == -1;
+	 *			}else{
+	 *				result ==  0;
+	 *	}
+	 */
 	@Override
 	public int compare(PositionPair pp1, PositionPair pp2) {
 		if(pp1.getManhattanDistance() > pp2.getManhattanDistance()){
--- src/roborally/utils/PositionPairComparatorEnergy.java
+++ src/roborally/utils/PositionPairComparatorEnergy.java
@@ -1,9 +1,28 @@
 package roborally.utils;
 
 import java.util.Comparator;
-
+/**
+ * Klasse met een speciale comperator in om positieparen te kunnen sorteren op de totale energe kost.
+ * 
+ * @author 	Bavo Goosens (1e bachelor informatica, r0297884), Samuel Debruyn (1e bachelor informatica, r0305472)
+ *
+ * @version 1.0
+ *  
+ */
 public class PositionPairComparatorEnergy implements Comparator<PositionPair>{
 
+	/**
+	 * Methode die een integer teruggeeft die aangeeft of positionpair1 zijn totale kost groter(1), kleiner(-1) of gelijk(0) is dan een tweede positiepaar.
+	 * 
+	 * @return 	int result 
+	 * 			|if(pp1.getTotalCost().getEnergy() > pp2.getTotalCost().getEnergy()){
+	 *				result ==  1;
+	 *			}else if(pp1.getTotalCost().getEnergy() < pp2.getTotalCost().getEnergy()){
+	 *				result == -1;
+	 *			}else{
+	 *				result ==  0;
+	 *	}
+	 */
 	@Override
 	public int compare(PositionPair pp1, PositionPair pp2) {
 		if(pp1.getTotalCost().getEnergy() > pp2.getTotalCost().getEnergy()){
