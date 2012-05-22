2a664377dd0fbc80002aed88599f921f9c80e484
Bavo Goosens <bavo.goosens@gmail.be>
2012-05-22 15:31:07 +0200

    program

--- src/roborally/utils/Program.java
+++ src/roborally/utils/Program.java
@@ -4,15 +4,16 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 
 
 public class Program {
 
 	private ArrayList<String> program = new ArrayList<>();
-	
+
 	private String prettyPrintLine = "";
-	
+
 	public Program(String path) throws FileNotFoundException{
 		File file = new File(path);
 		procesFile(file);
@@ -24,11 +25,19 @@ public class Program {
 			while (scan.hasNextLine()) {
 				this.prettyPrintLine.concat(scan.nextLine());
 			}
+			readProgram();
 		}catch (FileNotFoundException esc){
 			throw new FileNotFoundException();
 		}
 	}
-	
+
+	private void readProgram() {
+		String prettyPrintCopy = this.prettyPrintLine;
+		while (Pattern.matches("[a-zA-Z]", prettyPrintCopy)){
+			
+		}
+	}
+
 	public String getPrettyPrint(){
 		return this.prettyPrintLine;
 	}
