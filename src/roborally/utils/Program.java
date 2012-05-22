b7d0bbf5d9cf0b67e8e72d10335cfedf3fd85d05
Bavo Goosens <bavo.goosens@gmail.be>
2012-05-22 15:10:26 +0200

    program

--- src/roborally/utils/Program.java
+++ src/roborally/utils/Program.java
@@ -11,6 +11,8 @@ public class Program {
 
 	private ArrayList<String> program = new ArrayList<>();
 	
+	private String prettyPrintLine = "";
+	
 	public Program(String path) throws FileNotFoundException{
 		File file = new File(path);
 		procesFile(file);
@@ -18,18 +20,16 @@ public class Program {
 
 	private void procesFile(File file) throws FileNotFoundException {
 		try{
-			ArrayList<String> lines = new ArrayList<>();
 			Scanner scan = new Scanner(file);
 			while (scan.hasNextLine()) {
-				lines.add(scan.nextLine());
+				this.prettyPrintLine.concat(scan.nextLine());
 			}
-			this.program = getWord(lines);
 		}catch (FileNotFoundException esc){
 			throw new FileNotFoundException();
 		}
 	}
-
-	private ArrayList<String> getWord(ArrayList<String> lines) {
-		ArrayList<String> 
+	
+	public String getPrettyPrint(){
+		return this.prettyPrintLine;
 	}
 }
