1d1ee78a3b75d86cd16d24bce005f7d33a0351d5
Bavo Goosens <bavo.goosens@gmail.be>
2012-05-22 12:39:54 +0200

    SurpriseBox

--- src/roborally/model/SurpriseBox.java
+++ src/roborally/model/SurpriseBox.java
@@ -1,14 +1,45 @@
 package roborally.model;
 
-import roborally.property.Energy;
-import roborally.property.Weight;
+import java.util.ArrayList;
+import java.util.HashSet;
 
+import roborally.property.Energy;
+import roborally.property.Position;
+import roborally.property.Weight;
+/**
+ * 
+ * @author bavo
+ *
+ */
 public class SurpriseBox extends Item{
 
+	/**
+	 * 
+	 * @param weight
+	 */
 	public SurpriseBox(Weight weight) {
 		super(new Energy(0),weight);
 	}
+
+	/**
+	 * 
+	 */
+	@Override
+	protected void damage(){
+		ArrayList<Position> neighbours = this.getPosition().getNeighbours(this.getBoard());
+		for(Position position: neighbours){
+			HashSet<Entity> ents = this.getBoard().getEntityOnPosition(position);
+			if (ents != null)
+				for (Entity ent : ents)
+					ent.damage();
+		}
+		this.destroy();
+	}
+
+	@Override
+	public String toString() {
+		return "Position: " + this.getPosition().toString() + " Weight: " + this.getWeight().toString();
+	}
 	
-	
-	
+
 }
