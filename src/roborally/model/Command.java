516bdcfb14435a8ea6aafeb2cdd05c7b195f6ea4
Bavo Goosens <bavo.goosens@gmail.be>
2012-05-22 14:17:26 +0200

    program

---
+++ src/roborally/model/Command.java
@@ -0,0 +1,10 @@
+package roborally.model;
+
+import java.util.ArrayList;
+
+public class Command {
+	
+	static final String[] ALL = {"move","turn","shoot","pick-up-and-use","seq"
+			,"while","if","true","energy-at-least", "at-item","can-hit-robot", "wall"};
+
+}
