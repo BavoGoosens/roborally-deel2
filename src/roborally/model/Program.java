ece64a71728208088d997646e276958016e969da
Bavo Goosens <bavo.goosens@gmail.be>
2012-05-22 13:06:07 +0200

    program

--- src/roborally/model/Program.java
+++ src/roborally/model/Program.java
@@ -1,5 +1,21 @@
 package roborally.model;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Program {
+	
+	private ArrayList<Commands> program = new ArrayList<>();
+	
+	public Program(String file){
+		procesFile(file);
+	}
+
+	private void procesFile(String file) {
+		Scanner scan = new Scanner();
+		ArrayList<String> lines = new ArrayList<>();
+		
+		
+	}
 
 }
