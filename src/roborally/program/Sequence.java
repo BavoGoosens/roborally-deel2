65a268ce81ca32e9a76959cc520be78aa11f13e2
Bavo Goosens <bavo.goosens@gmail.be>
2012-05-23 15:47:23 +0200

    namn verbterd

---
+++ src/roborally/program/Sequence.java
@@ -0,0 +1,39 @@
+package roborally.program;
+
+import java.util.ArrayList;
+
+import roborally.model.Robot;
+
+public class Sequence extends Command {
+	
+	public boolean enterd = false;
+	
+	public Sequence(String substring) {
+		makeBody(substring);
+	}
+
+	private void makeBody(String substring) {
+		substring = substring.trim();
+		while (substring.length() > 3){
+			substring = substring.trim();
+			substring = substring.substring(substring.indexOf('('));
+			String cmd = haalHaakskesWeg(substring);
+			getBody().add(getFirstCommand(substring));
+			substring = substring.substring(cmd.length() + 3);
+		}
+	}
+
+	@Override
+	public void execute (Robot robot){
+		for (int i = 0; i < getBody().size(); i++){
+			Command comm = getBody().get(i);
+			comm.execute(robot);
+		}
+	}
+	
+	private ArrayList<Command> body = new ArrayList<>();
+
+	public ArrayList<Command> getBody() {
+		return this.body;
+	}
+}
---
+++ src/roborally/program/Statement.java
@@ -0,0 +1,69 @@
+package roborally.program;
+
+import java.util.HashSet;
+
+import roborally.model.Entity;
+import roborally.model.Item;
+import roborally.model.Robot;
+
+public class Statement extends Command {
+	
+	public boolean enterd = false;
+	
+	private int turnDir;
+
+	public int getTurnDir() {
+		return this.turnDir;
+	}
+
+	public void setTurnDir(int turnDir) {
+		this.turnDir = turnDir;
+	}
+
+	public Statement(BasicEnum en) {
+		setEnumOfThis(en);
+	}
+
+	public Statement(BasicEnum turn, String string) {
+		setEnumOfThis(turn);
+		if(string.contains("counterclockwise")){
+			this.turnDir = 1;
+		}
+		this.turnDir = 0;
+	}
+
+	private void setEnumOfThis(BasicEnum en) {
+		this.enumOfThis = en;
+	}
+
+	private BasicEnum enumOfThis;
+	
+	public BasicEnum getEnumOfThis() {
+		return this.enumOfThis;
+	}
+
+	@Override
+	public void execute(Robot robot){
+		if (this.getEnumOfThis() == BasicEnum.MOVE){
+			robot.move();
+		}else if (this.getEnumOfThis() == BasicEnum.SHOOT){
+			robot.shoot();
+		}else if (this.getEnumOfThis() == BasicEnum.TURN){
+			if (getTurnDir() == 1){
+				robot.turnCounterClockWise();
+			}
+			robot.turnClockWise();
+		}else{
+			HashSet<Entity> ents = robot.getBoard().getEntityOnPosition(robot.getPosition());
+			for (Entity ent : ents){
+				if (ent instanceof Item){
+					robot.pickUp((Item) ent);
+					robot.use((Item) ent);
+					break;
+				}
+			}
+		}
+		
+	}
+
+}
